6ed524f2262d6719df26426913fabbf0ac1ae308
wally_yoon <bluegmlduf2@naver.com>
2023-05-18 23:40:19 +0900

    fix:psr-autoload에 맞춰 경로변경

---
+++ example/Test.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__.'/../src/ExtractCounty.php';
+
+use ExtractingCountryFromText\ExtractCounty;
+
+try {
+    $country = new ExtractCounty([
+        "searchWord" => "korea",
+        "searcItem" => "name",
+        "fullSearch" => false,
+        "language" => "ko",
+    ]);
+
+    var_dump($country->getCountryFullName());
+    echo "<br>";
+    var_dump($country->getCountryTwoLetter());
+    echo "<br>";
+    var_dump($country->getCountryThreeLetter());
+} catch (\Throwable $th) {
+    echo $th->getMessage();
+}
